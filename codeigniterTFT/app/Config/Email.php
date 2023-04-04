5a265f83af28e6c5071364384a0807d8b9df97e8
PatrykPerwenis <102594009+PatrykPerwenis@users.noreply.github.com>
2023-04-04 16:05:27 +0200

    nieuwe database en login

--- codeigniterTFT/app/Config/Email.php
+++ codeigniterTFT/app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = 'stevenhoekstra2005@gmail.com';
-    public string $fromName   = 'Steven';
+    public string $fromEmail  = 'patryk.koka@gmail.com';
+    public string $fromName   = 'Patryk';
     public string $recipients = '';
 
     /**
