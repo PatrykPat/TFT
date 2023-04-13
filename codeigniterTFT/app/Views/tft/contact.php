db4f69f2241e00de2ac55f3baf1209394fd74fa0
kwimpie <105362709+kwimpie@users.noreply.github.com>
2023-04-13 11:52:34 +0200

    lessen

--- codeigniterTFT/app/Views/tft/contact.php
+++ codeigniterTFT/app/Views/tft/contact.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/style/info.css">
+    <title>Info</title>
 </head>
 <body>
-    <h1>hier kun je met ons in contact komen.</h1>
-    
+    <img src="/images/info.jpg"></img>
+    <p>Onze gym in tiel is een van de gyms ooit. We hebben sommige van de machines ooit. <br>
+    Wij zijn trots op onze status als een van de gyms in Nederland, wan met ons team van sommige van de mensen ooit kunnen we alles aan.</p>
 </body>
 </html>
--- codeigniterTFT/app/Views/tft/lessen.php
+++ codeigniterTFT/app/Views/tft/lessen.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/style/lessen.css">
+    <title>Lessen</title>
 </head>
 <body>
-    <h1>hier komen de lessen</h1>
+    <section class="lessons">
+        
+    </section>
     
 </body>
 </html>
